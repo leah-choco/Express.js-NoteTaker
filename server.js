052d7901c0b873b42bee4ba11de9b34ed6f59e56
Your Name <you@example.com>
2022-10-10 20:59:31 -0500

    Worked with tutor, setting up server.js and understanding routes

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const fs = require("fs");
+const util = require("util");
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+  readFile("./db/db.json", "utf-8")
+  .then
+})
+
+
+
+
+//HTMl route 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+// Wildcard route
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
+//CRUD C=create(POST), R=read(GET), U=update(PUT), D=Delete
